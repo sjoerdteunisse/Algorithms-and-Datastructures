d11d2f460f1d2ae0cde72412097de8af5d981cb5
Axon Robotics <sjoerdteunisse@gmail.com>
2019-09-15 12:14:42 +0200

    notation cs added for  information on Big O

---
+++ ADS.Core/Helpers/Notation.cs
@@ -0,0 +1,35 @@
+﻿namespace ADS.Core.Helpers
+{
+    public class Notation
+    {
+        //Data Structure         Average cases                                 Worst cases
+        //Insert    Delete      Search      Insert  Delete  Search
+        //Array/stack/queue     O(1)        O(1)        O(n)        O(1)    O(1)    O(n)
+        //Linked list           O(1)        O(1)        O(n)        O(1)    O(1)    O(n)
+        //Doubly linked list    O(1)        O(1)        O(n)        O(1)    O(1)    O(n)
+        //Hash table            O(1)        O(1)        O(1)        O(n)    O(n)    O(n)
+        //Binary search tree    O(log(n))   O(log(n))   O(log(n))   O(n)    O(n)    O(n)
+
+
+        //Best Average Worst
+        //    Quick Sort Ω(n log (n) ) Θ(n log(n) ) O(n2 )
+        //Merge Sort Ω(n log (n) ) Θ(n log(n) ) O(n log(n) )
+        //Timsort Ω(n ) Θ(n log(n) ) O(n log(n) )
+        //Heap Sort Ω(n log (n) ) Θ(n log(n) ) O(n log(n) )
+        //Bubble Sort Ω(n ) Θ(n2 ) O(n2 )
+        //Insertion Sort Ω(n ) Θ(n2 ) O(n2 )
+        //Selection Sort Ω(n2 ) Θ(n2 ) O(n2 )
+        //Tree Sort Ω(n log (n) ) Θ(n log(n) ) O(n2 )
+        //Shell Sort Ω(n log (n) ) Θ(n (log(n) )2) O(n (log(n) )2)
+        //Bucket Sort Ω(n+k ) Θ(n+k ) O(n2 )
+        //Radix Sort Ω(nk ) Θ(nk ) O(nk )
+        //Counting Sort Ω(n+k ) Θ(n+k ) O(n+k )
+        //Cubesort Ω(n ) Θ(n log(n) ) O(n log(n) )
+        //Smooth Sort Ω(n ) Θ(n log(n) ) O(n log(n) )
+        //Tournament Sort - Θ(n log(n) ) O(n log(n) )
+        //Stooge sort - - O(n log 3 /log 1.5 )
+        //Gnome/Stupid sort Ω(n ) Θ(n2 ) O(n2 )
+        //Comb sort Ω(n log (n) ) Θ(n2/p2) O(n2 )
+        //Odd – Even sort Ω(n ) - O(n2 )
+    }
+}
